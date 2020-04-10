b856f8c9ebbef78430edfe4cc071f41171aff107
Ilya Doroshenko <ilya.dor201232@gmail.com>
2020-04-10 23:51:16 +0300

    Избавил обработчики сигнала от тяжелых операций, перенес их в другое место Избавился от лишнего глобального массива char** Убрал из цикла много затратных операций. Теперь они выполняются один раз Избавился от своей функции Split(), использую strtok(), занимает гораздо меньше места. Закрываю все дескрипторы чтобы не было утечек

--- daemon.c
+++ daemon.c
@@ -5,12 +5,9 @@
  * 	SIGINT - выполнить команду из файла и записать результат в 'out.txt'.
  * 	SIGTERM - закончить работу демона.
  * 
- * Столкнулся с проблемой передачи аргументов мейна в обработчик сигнала.
- * Решил ее, сделав массив строк с этими аргументам глобальным.
- * Сомневаюсь в корректности этого решения, но в данной задаче это 
- * не критично на мой взгяд.
  * 
- * gcc daemon.c -o && daemon && ./daemon && kill -SIGINT $(pidof daemon) && kill -SIGTERM $(pidof daemon) && cat out.txt
+ * gcc daemon.c -o daemon && ./daemon cmd.txt && kill -SIGINT $(pidof daemon) && kill -SIGTERM $(pidof daemon)
+ * cat out.txt
  */
 
 
@@ -27,66 +24,82 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <syslog.h>
-char** argv;
-char** split(char* buf, char ch);
+
+int sigterm_h = 0;
+int sigint_h = 0;
 
 void sigterm_handler(int signum){
-	char logText[] = "Daemon caught SIGTERM and dead\n";
-	int fd = open("log.txt", O_CREAT|O_RDWR, S_IRWXU);
-	lseek(fd, 0, SEEK_END);
-	write(fd, logText, sizeof(logText));
-	
-	syslog(LOG_NOTICE, "Daemon caught SIGTERM and dead");
-	
-	exit(0);
+    sigterm_h = 1;
 }
 
 void sigint_handler(int signum)
-{		
-	char logText[] = "Daemon caught SIGINT\n";						// Запись о перехвате в файл log.txt
-	int fd = open("log.txt", O_CREAT|O_RDWR, S_IRWXU);  			//	
-	lseek(fd, 0, SEEK_END);											//
-	write(fd, logText, sizeof(logText));							//
-	
-	
-	syslog(LOG_NOTICE, "Daemon caught SIGINT");						// Запись о перехвате в syslog.
-	
-	pid_t p;
-	if((p = fork()) == 0 ){											// Форкаю, чтобы демон не умирал после вызова execv
-		char buf[256] = "";
-		
-		int fd_read = open(argv[1], O_CREAT|O_RDWR, S_IRWXU);      
-		int count = read(fd_read, buf, sizeof(buf));
-		
-		buf[count-1] = '\0';										// Буффер с командой
-		
-		char** res = split(buf, ' ');								// Разбиваю строку из входного файла на множество строк, если есть дополнительные параметры
-		
-		int fd_write = open("out.txt", O_CREAT|O_RDWR, S_IRWXU);	
-
-		close(1);
-		dup2(fd_write, 1);											//Переопределяем дескриптор на вывод в файл
-		execv(res[0], res);
-	}
-	
-}
-int Daemon(void){
-	openlog("MyDaemon", LOG_PID, LOG_DAEMON);
-	while(1){
-		signal(SIGTERM, sigterm_handler);							// Перехват -SIGTERM - закончить выполнение демона
-		signal(SIGINT, sigint_handler);								// Перехват -SIGINT - вызвать команду из входного файла. Результат в out.txt
-		pause();
-	}
-}
-
-int main(int argc, char* argv1[])
 {
-	argv = argv1;
+    sigint_h = 1;
+}
+int Daemon(char** argv){
+	openlog("MyDaemon", LOG_PID, LOG_DAEMON);
+    signal(SIGTERM, sigterm_handler);							// Перехват -SIGTERM - закончить выполнение демона
+    signal(SIGINT, sigint_handler);								// Перехват -SIGINT - вызвать команду из входного файла. Результат в out.txt
+	char logText[] = "Daemon caught SIGINT\n";
+	char buf[256] = "";
+
+	int fd_read = open(argv[1], O_CREAT|O_RDWR, S_IRWXU);
+	int count = read(fd_read, buf, sizeof(buf));
+	close(fd_read);
+	buf[count-1] = '\0';										
+
+	char** res;
+	char* tmp;
+	int i = 0;
+	count = 0;
+	while(tmp = strtok(buf + i,' ')){
+		res[count++] = tmp;
+		i += sizeof(tmp);
+	}
+    while(1) {
+        if(sigint_h) {
+            int fd = open("log.txt", O_CREAT|O_RDWR, S_IRWXU);  			
+            lseek(fd, 0, SEEK_END);											
+            write(fd, logText, sizeof(logText));							
+            close(fd);
+            syslog(LOG_NOTICE, "Daemon caught SIGINT");						
+
+            pid_t p;
+            if((p = fork()) == 0 ){											// Форкаю, чтобы демон не умирал после вызова execv
+
+                int fd_write = open("out.txt", O_CREAT|O_RDWR, S_IRWXU);
+
+                close(1);
+                dup2(fd_write, 1);											//Переопределяем дескриптор на вывод в файл
+                execv(res[0], res);
+            }
+
+            sigint_h = 0;
+        }
+        if(sigterm_h) {
+
+            char logText[] = "Daemon caught SIGTERM and dead\n";
+            int fd = open("log.txt", O_CREAT|O_RDWR, S_IRWXU);
+            lseek(fd, 0, SEEK_END);
+            write(fd, logText, sizeof(logText));
+
+            syslog(LOG_NOTICE, "Daemon caught SIGTERM and dead\n");
+			close(fd);
+            exit(0);
+        }
+
+        pause();
+    }
+}
+
+
+int main(int argc, char* argv[])
+{
 	pid_t pid;
 	if(pid = fork() == 0)  
 	{
 		setsid();
-		Daemon();
+		Daemon(argv);
 	}
 	else if(pid < 0){
 		printf("Error!");
@@ -94,35 +107,4 @@ int main(int argc, char* argv1[])
 	}
 
 	return 0;
-}
-char** split(char* buf, char ch) {
-	char* tmp = (char*)malloc(sizeof(char) * 100);
-	char** array;
-	int n = strlen(buf);
-	int N = 0;
-	int count = 0;
-	int j = 0;
-	for (int i = 0; i <= n; i++) {
-		count++;
-		if (buf[i] == '\0' || buf[i] == ch) {
-			{
-				for (int k = 0; k < count-1; k++) {
-					tmp[k] = buf[k + j];
-				}
-				tmp[count-1] = '\0';
-				j = i + 1;
-				
-				array[N] = (char*)malloc(sizeof(tmp));
-				for(int q = 0; q <= sizeof(tmp); q++)
-					array[N][q] = tmp[q];
-				
-				count = 0;
-				for(int q = 0; q <= sizeof(tmp); q++)
-					tmp[q] = '\0';
-			}
-			(N)++;
-		}
-	}
-	array[N] = NULL;
-	return array;
 }
